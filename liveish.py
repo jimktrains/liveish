a697d2cb96f9f5b83f7bad71989798b780315e3a
Jim Keener <jim@jimkeener.com>
2017-11-25 01:05:49 -0600

    Adds a ton of complexity....but fixes a lot of subtle bugs

--- liveish.py
+++ liveish.py
@@ -34,6 +34,8 @@ class PTYFork:
         os.set_blocking(self.child_fd, False) 
 
         if size is not None:
+            if 'x' in size:
+                size = list(map(int, size.split('x',1)))
             sizebuf = array.array('h', list(reversed(size)))
             fcntl.ioctl(self.child_fd, termios.TIOCSWINSZ, sizebuf)
 
@@ -46,11 +48,6 @@ class PTYFork:
     def write(self, b):
         return os.write(self.child_fd, b)
 
-    def set_nonblocking(self, fd):
-        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-        fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-
-
     def fileno(self):
         return self.child_fd
 
@@ -60,9 +57,6 @@ class PTYFork:
     def __exit__(self, type, value, traceback):
         os.kill(self.child_pid, signal.SIGTERM)
 
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 class QWaitForOutput:
     def __init__(self, wait_for_output = 2):
@@ -108,6 +102,8 @@ class QEnter(QInput):
         super().__init__("^J", 0, 0)
 
 def gauss_bound(µ, σ, minn, maxx):
+    µ = float(µ)
+    σ = float(σ)
     randnum = random.gauss(µ, σ)
     while randnum < minn or randnum < (µ + (maxx*σ)):
         randnum = random.gauss(µ, σ)
@@ -117,100 +113,308 @@ class MQPID:
     def __init__(self, pid):
         self.pid = pid
 
-def worker(command, cmdq, metaq, exit_now, echo_callback, noecho_callback, application_launched_barrier):
-    with PTYFork(command) as terminal:
-        metaq.put(MQPID(terminal.child_pid))
-        application_launched_barrier.wait()
+def worker(command, cmdq, metaq, exit_now, not_accepting_input, echo_callback, noecho_callback, viewport, env, application_launched_barrier):
+    try:
+        with PTYFork(command, env, viewport) as terminal:
+            metaq.put(MQPID(terminal.child_pid))
+            application_launched_barrier.wait()
 
-        echo = True
-        sleep_until = False
-        last_input = False
-        last_output_at = time.time()
-        wait_for_output = False
-        exit_after_output = False
-        while True and not exit_now.is_set():
-            bytes_from_terminal = terminal.read(128)
-            if bytes_from_terminal is not None:
-                last_output_at = time.time()
-                if echo:
-                    echo_callback(bytes_from_terminal)
-                else:
-                    noecho_callback(bytes_from_terminal)
-
-            if wait_for_output != False:
-                if (time.time() - last_output_at) > wait_for_output:
-                    wait_for_output = False
-                    if exit_after_output:
-                        break
-            elif last_input != False:
-                if last_input.next_time < time.time():
-                    if last_input.delay_mean == 0:
-                        terminal.write(last_input.str_in.encode('utf8'))
-                        last_input = False
+            echo = True
+            sleep_until = False
+            last_input = False
+            last_output_at = time.time()
+            wait_for_output = False
+            exit_after_output = False
+            while True and not exit_now.is_set():
+                bytes_from_terminal = terminal.read(128)
+                if bytes_from_terminal is not None:
+                    last_output_at = time.time()
+                    if echo:
+                        echo_callback(bytes_from_terminal)
                     else:
-                        terminal.write(last_input.str_in[0].encode('utf8'))
-                        wait_time = gauss_bound(last_input.delay_mean, last_input.delay_std, 0, 3) / 1000.0
-                        last_input.next_time = time.time() + wait_time
-                        last_input.str_in = last_input.str_in[1:]
-                        if len(last_input.str_in) == 0:
-                            last_input = False
-            elif sleep_until != False:
-                if time.time() > sleep_until:
-                    sleep_until = False
-            else:
-                sleep_until = False
-                if not cmdq.empty():
-                    cmd = cmdq.get()
-                    if isinstance(cmd, QWaitForOutput):
-                        wait_for_output = cmd.wait_for_output
-                        exit_after_output = isinstance(cmd, QExit)
-                    elif isinstance(cmd, QEcho):
-                        echo = cmd.echo
-                    elif isinstance(cmd, QSleep):
-                        sleep_until = cmd.until
-                    elif isinstance(cmd, QInput):
-                        last_input = cmd
+                        noecho_callback(bytes_from_terminal)
 
+                time_since_last_output = time.time() - last_output_at
+
+                if wait_for_output != False:
+                    not_accepting_input.set()
+                    if time_since_last_output > wait_for_output:
+                        wait_for_output = False
+                        not_accepting_input.clear()
+                        if exit_after_output:
+                            break
+                elif last_input != False:
+                    if last_input.next_time < time.time():
+                        if last_input.delay_mean == 0:
+                            terminal.write(last_input.str_in.encode('utf8'))
+                            last_input = False
+                        else:
+                            not_accepting_input.set()
+                            terminal.write(last_input.str_in[0].encode('utf8'))
+                            wait_time = gauss_bound(last_input.delay_mean, last_input.delay_std, 0, 3) / 1000.0
+                            last_input.next_time = time.time() + wait_time
+                            last_input.str_in = last_input.str_in[1:]
+                            if len(last_input.str_in) == 0:
+                                last_input = False
+                                not_accepting_input.clear()
+                elif sleep_until != False:
+                    if time.time() > sleep_until:
+                        sleep_until = False
+                else:
+                    sleep_until = False
+                    if not cmdq.empty():
+                        cmd = cmdq.get()
+                        if isinstance(cmd, QWaitForOutput):
+                            wait_for_output = float(cmd.wait_for_output)
+                            exit_after_output = isinstance(cmd, QExit)
+                        elif isinstance(cmd, QEcho):
+                            echo = cmd.echo
+                        elif isinstance(cmd, QSleep):
+                            sleep_until = cmd.until
+                        elif isinstance(cmd, QInput):
+                            last_input = cmd
+    finally:
+        exit_now.set()
 
 config = {
     'command': '/usr/bin/env bash',
     'host': '127.0.0.1',
     'port': '7890',
     'auto-delay': 0.500,
-    'type.average_delay': 50,
-    'type.stddev_delay': 50,
+    'type.average_delay': 20,
+    'type.stddev_delay': 30,
+    'viewport': "80x24",
+    'env': {}
 }
 
 q = queue.Queue()
 m = queue.Queue()
-hostport = (config['host'], int(config['port']))
-print("Sending output to " + ":".join(map(str, hostport)))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+def normalize_line(line):
+    line = line.strip()
+    if len(line) == 0:
+        line = "%pause-script"
+    elif line[0] == '#':
+        comment = ""
+        if len(line) != 1:
+            comment = line[1:]
+        line = "%comment " + comment
+    elif line[0] == '!':
+        line = line[1:]
+        line = "%config " + line
+    elif line[0] == '>':
+        if len(line) == 1:
+            line = ">\^J"
+        rest = line[2:]
+        if line[1] == '\\':
+            line = "%type " + rest
+        elif line[1] == '+':
+            line = "%auto-type-run " + rest
+        else:
+            line = "%type-run " + rest
+    elif line[0] == '+':
+        cmd = line[2:]
+        line = "%auto-run-no-echo " + cmd
+    elif line[0] == '$':
+        line = "%wait-for-output"
+    elif line[0] == '@':
+        duration = float(line[2:])
+        line = "%sleep " + str(duration)
+
+    return line
+
+vt100_escape_codes = {
+"setnl": "^[[20h",
+"setappl": "^[[?1h",
+"setcol": "^[[?3h",
+"setsmooth": "^[[?4h",
+"setrevscrn": "^[[?5h",
+"setorgrel": "^[[?6h",
+"setwrap": "^[[?7h",
+"setrep": "^[[?8h",
+"setinter": "^[[?9h",
+
+"setlf": "^[[20l",
+"setcursor": "^[[?1l",
+"setvt52": "^[[?2l",
+"resetcol": "^[[?3l",
+"setjump": "^[[?4l",
+"setnormscrn": "^[[?5l",
+"setorgabs": "^[[?6l",
+"resetwrap": "^[[?7l",
+"resetrep": "^[[?8l",
+"resetinter": "^[[?9l",
+
+"altkeypad": "^[=",
+"numkeypad": "^[>",
+
+"setukg0": "^[(A",
+"setukg1": "^[)A",
+"setusg0": "^[(B",
+"setusg1": "^[)B",
+"setspecg0": "^[(0",
+"setspecg1": "^[)0",
+"setaltg0": "^[(1",
+"setaltg1": "^[)1",
+"setaltspecg0": "^[(2",
+"setaltspecg1": "^[)2",
+
+"setss2": "^[N",
+"setss3": "^[O",
+
+"modesoff": "^[[m",
+"modesoff": "^[[0m",
+"bold": "^[[1m",
+"lowint": "^[[2m",
+"underline": "^[[4m",
+"blink": "^[[5m",
+"reverse": "^[[7m",
+"invisible": "^[[8m",
+
+"setwin": "^[[;r",
+
+#"cursorup(n)": "^[[A",
+#"cursordn(n)": "^[[B",
+#"cursorrt(n)": "^[[C",
+#"cursorlf(n)": "^[[D",
+"cursorhome": "^[[H",
+"cursorhome": "^[[;H",
+#"cursorpos(v,h)": "^[[;H",
+"hvhome": "^[[f",
+"hvhome": "^[[;f",
+#"hvpos(v,h)": "^[[;f",
+"index": "^[D",
+"revindex": "^[M",
+"nextline": "^[E",
+"savecursor": "^[7",
+"restorecursor": "^[8",
+
+"tabset": "^[H",
+"tabclr": "^[[g",
+"tabclr": "^[[0g",
+"tabclrall": "^[[3g",
+
+"dhtop": "^[#3",
+"dhbot": "^[#4",
+"swsh": "^[#5",
+"dwsh": "^[#6",
+
+"cleareol": "^[[K",
+"cleareol": "^[[0K",
+"clearbol": "^[[1K",
+"clearline": "^[[2K",
+
+"cleareos": "^[[J",
+"cleareos": "^[[0J",
+"clearbos": "^[[1J",
+"clearscreen": "^[[2J",
+
+"devstat": "^[5n",
+"termok": "^[0n",
+"termnok": "^[3n",
+
+"getcursor": "^[6n",
+"cursorpos": "^[;R",
+
+"ident": "^[[c",
+"ident": "^[[0c",
+"gettype": "^[[?1;0c",
+
+"reset": "^[c",
+
+"align": "^[#8",
+"testpu": "^[[2;1y",
+"testlb": "^[[2;2y",
+"testpurep": "^[[2;9y",
+"testlbrep": "^[[2;10y",
+
+"ledsoff": "^[[0q",
+"led1": "^[[1q",
+"led2": "^[[2q",
+"led3": "^[[3q",
+"led4": "^[[4q",
+
+
+"setansi": "^[<",
+
+"altkeypad": "^[=",
+"numkeypad": "^[>",
+
+"setgr": "^[F",
+"resetgr": "^[G",
+
+"cursorup": "^[A",
+"cursordn": "^[B",
+"cursorrt": "^[C",
+"cursorlf": "^[D",
+"cursorhome": "^[H",
+#"cursorpos(v,h)": "^[",
+"revindex": "^[I",
+
+"cleareol": "^[K",
+"cleareos": "^[J",
+
+"ident": "^[Z",
+"identresp": "^[/Z",
+"PF1": "^[OP",
+"PF2": "^[OQ",
+"PF3": "^[OR",
+"PF4": "^[OS",
+"up": "^[A",
+"down": "^[B",
+"right": "^[C",
+"left": "^[D",
+}
 
 with open(sys.argv[1]) as scriptfile:
     told = 0
     line = scriptfile.readline().strip()
-    while len(line) > 0 and line[0] == '!':
-        (key, value) = line.split(" ", 1)
-        key = key[1:]
-        config[key] = value
+    waiting_on_typing = False
+    # .tell doesn't seem to work once an iterator is started, hence not
+    # using a for-loop here
+    while len(line) > 0:
+        if len(line) == 0:
+            continue
+        elif line[0] == '!':
+            (key, value) = line.split(" ", 1)
+            key = key[1:]
+            config[key] = value
+        elif line[0:4] == '%env':
+            (key, value) = line[5:].split(" ", 1)
+            config['env'][key] = value
+        elif line[0] == '#':
+            continue
+        else:
+            break
         told = scriptfile.tell()
-        line = scriptfile.readline()
+        line = scriptfile.readline().strip()
     # Rewind a little so that we can start at the script
     scriptfile.seek(told)
 
-
     application_launched_barrier = Barrier(2, timeout=5)
     exit_now = Event()
+    not_accepting_input = Event()
+
+
     thread = Thread(target = worker, args = (
         config['command'],
         q,
         m,
         exit_now,
+        not_accepting_input,
         lambda x : sock.sendto(x, hostport),
         lambda x : (sys.stdout.buffer.write(x), sys.stdout.buffer.flush()),
+        config['viewport'],
+        config['env'],
         application_launched_barrier
     ))
     thread.start()
+
+    hostport = (config['host'], int(config['port']))
+    print("Sending output to " + ":".join(map(str, hostport)))
 
     application_launched_barrier.wait()
 
@@ -220,46 +424,109 @@ with open(sys.argv[1]) as scriptfile:
             print(config['command'] + " started at pid " + str(metadata.pid))
 
     try:
+        q.put(QWaitForOutput(config['auto-delay']))
         for line in scriptfile:
-            line = line.strip()
-            if len(line) == 0:
-                print()
-            elif line[0] == '#':
-                continue
-            elif line[0] == '!':
-                (key, value) = line.split(" ", 1)
-                key = key[1:]
-                config[key] = value
-            elif line[0] == '>':
-                cmd = line[2:]
-                if (line[1] == ' '):
-                    input("type? " + cmd)
-                elif (line[1] == '\\'):
-                    input("type&run? " + cmd)
-                elif (line[1] == '+'):
-                    print("auto-run? " + cmd)
-                q.put(QInput(cmd, config['type.average_delay'], config['type.stddev_delay']))
-                if (line[1] == ' '):
-                    input("run?")
-                    q.put(QEnter())
-                if (line[1] == '+'):
-                    q.put(QEnter())
-                    time.sleep(float(config['auto-delay']))
-            elif line[0] == '+':
-                cmd = line[2:]
-                print("auto command " + cmd)
-                q.put(QEcho(False))
-                q.put(QInput(cmd))
-                q.put(QEnter())
-                q.put(QWaitForOutput(config['auto-delay']))
-                q.put(QEcho(True))
-                time.sleep(config['auto-delay'])
-            elif line[0] == '@':
-                duration = float(line[2:])
-                print("sleeping for " + str(duration))
-                time.sleep(duration)
+            if exit_now.is_set():
+                break
+            line = normalize_line(line)
+
+            cmd = None
+            if line[0] == '%':
+                cmd = line
+                if ' ' in line:
+                    cmd, line = line.split(' ', 1)
+                cmd = cmd[1:]
+
+            keep_trying = True
+            notified = False
+            waiting_notified = False
+            while keep_trying and not exit_now.is_set():
+                keep_trying = False
+
+                if cmd == 'pause-script':
+                    input('<paused>')
+                    print()
+                elif cmd == "comment":
+                    pass
+                elif cmd == "config":
+                    (key, value) = line.split(" ", 1)
+                    print(key, value)
+                    config[key] = value
+                elif cmd and "type" in cmd:
+                    if not q.empty() or not_accepting_input.is_set():
+                        keep_trying = True
+                        time.sleep(0.1)
+                        if not notified:
+                            print("<Waiting on to accept input>")
+                            notified = True
+                    else:
+                        if cmd == "type-run":
+                            input("type-run! " + line)
+                            q.put(QInput(line, config['type.average_delay'], config['type.stddev_delay']))
+                            input("run?")
+                            q.put(QEnter())
+                            q.put(QWaitForOutput(config['auto-delay']))
+                        elif cmd == "auto-type-run":
+                            print("auto-run! " + line)
+                            q.put(QInput(line, config['type.average_delay'], config['type.stddev_delay']))
+                            q.put(QEnter())
+                            q.put(QWaitForOutput(config['auto-delay']))
+                            time.sleep(float(config['auto-delay']))
+                        elif cmd == "type-auto-run":
+                            input("type-auto-run? " + line)
+                            q.put(QInput(line, config['type.average_delay'], config['type.stddev_delay']))
+                            q.put(QEnter())
+                            q.put(QWaitForOutput(config['auto-delay']))
+                        elif cmd == "type":
+                            input("type-only? " + line)
+                            q.put(QInput(line, config['type.average_delay'], config['type.stddev_delay']))
+                            if (line == "^["):
+                                q.put(QInput('^@'))
+                            q.put(QWaitForOutput(config['auto-delay']))
+                elif cmd == "auto-run-no-echo":
+                    if not q.empty() or not_accepting_input.is_set():
+                        keep_trying = True
+                        time.sleep(0.1)
+                        if not notified:
+                            print("<Waiting on to accept input>")
+                            notified = True
+                    else:
+                        print("auto command " + line)
+                        q.put(QEcho(False))
+                        q.put(QInput(line))
+                        q.put(QEnter())
+                        q.put(QWaitForOutput(config['auto-delay']))
+                        q.put(QEcho(True))
+                elif cmd == "wait-for-output":
+                    q.put(QWaitForOutput(config['auto-delay']))
+                elif cmd == "sleep":
+                    duration = float(line)
+                    print("sleeping for " + str(duration))
+                    time.sleep(duration)
+                elif cmd == "vt100":
+                    if line in vt100_escape_codes:
+                        q.put(QInput(vt100_escape_codes[line]))
+                elif cmd == "exit":
+                    if not q.empty() or not_accepting_input.is_set():
+                        keep_trying = True
+                        time.sleep(0.1)
+                        if not notified:
+                            print("<Waiting on to accept input>")
+                            notified = True
+                    else:
+                        exit_now.set()
+                else:
+                    print(line.strip())
+        notified = False
+        keep_trying = True
+        while keep_trying and not exit_now.is_set():
+            if not q.empty() or not_accepting_input.is_set():
+                time.sleep(0.1)
+                if not notified:
+                    print("<Waiting on to accept input>")
+                    notified = True
             else:
-                print(line.strip())
+                keep_trying = False
         input("exit?")
     except KeyboardInterrupt:
         pass
